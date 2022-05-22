e4b5eb8850a47760cee5f42b8718fee095364571
Андрей Тренкаль <andrey.trenkal@open.ru>
2022-05-22 13:51:53 +0200

    feat: added dummy version of multiply function
    added dummy version of multiply function

--- cpp/react-native-env-js.cpp
+++ cpp/react-native-env-js.cpp
@@ -6,6 +6,20 @@ using namespace std;
 
 void installSequel(jsi::Runtime &rt) {
     cout << "Installing JSI binding" << endl;
+    
+    jsi::Function multiply = jsi::Function::createFromHostFunction(
+       rt,
+       jsi::PropNameID::forAscii(rt, "multiply"),
+       0,
+       [](jsi::Runtime &rt,
+          const jsi::Value & thisValue,
+          const jsi::Value *args,
+          size_t count) -> jsi::Value {
+              return jsi::Value(42);
+          }
+       );
+    
+    rt.global().setProperty(rt, "multiply", multiply);
 }
 
 void cleanUpSequel() {
