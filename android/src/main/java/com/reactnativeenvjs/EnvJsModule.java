3f08010c3cb2bc02760f6a41288b6793463f573e
Андрей Тренкаль <trenkal_ai@open.ru>
2022-07-19 01:16:10 +0200

    feat: added jsi instead native module for android
    added jsi instead native module for android

--- android/src/main/java/com/reactnativeenvjs/EnvJsModule.java
+++ android/src/main/java/com/reactnativeenvjs/EnvJsModule.java
@@ -7,42 +7,39 @@ import androidx.annotation.NonNull;
 import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
 @ReactModule(name = EnvJsModule.NAME)
 public class EnvJsModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "EnvJsModule";
+  public static final String NAME = "EnvJsModule";
 
-    public EnvJsModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-    }
-
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
-    }
-
-    static {
-        try {
-            // Used to load the 'native-lib' library on application startup.
-            System.loadLibrary("EnvJs");
-        } catch (Exception ignored) {
-        }
-    }
-
-    private native void nativeInstall(long jsi);
-
-  @ReactMethod(isBlockingSynchronousMethod = true)
-  public boolean install() {
+  static {
     try {
-      JavaScriptContextHolder jsContext = getReactApplicationContext().getJavaScriptContextHolder();
-      nativeInstall(jsContext.get());
-      return true;
-    } catch (Exception exception) {
-      Log.e(NAME, "Failed to install EnvJS JSI Bindings!", exception);
-      return false;
+      System.loadLibrary("EnvJs");
+    } catch (Exception ignored) {
+    }
+  }
+
+  public EnvJsModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+  private native void nativeInstall(long jsi);
+
+  public void installLib(JavaScriptContextHolder reactContext) {
+
+    if (reactContext.get() != 0) {
+      this.nativeInstall(
+        reactContext.get()
+      );
+    } else {
+      Log.e("EnvJsModule", "JSI Runtime is not available in debug mode");
     }
   }
 }
--- example/android/app/src/main/java/com/example/reactnativeenvjs/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativeenvjs/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -28,8 +29,12 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for EnvJsExample:
           // packages.add(new MyReactNativePackage());
-          packages.add(new EnvJsPackage());
           return packages;
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new EnvJsPackage();
         }
 
         @Override
